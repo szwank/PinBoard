359290716339f096c675f2e742f51230d12bf5bd
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2023-06-02 19:50:55 +0200

    Add black as project formatter
    Format project using black
    Add black to requirements

--- PinBoard/wsgi.py
+++ PinBoard/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDoList.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ToDoList.settings")
 
 application = get_wsgi_application()
