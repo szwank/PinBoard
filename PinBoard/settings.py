63a37ad1cf33ef8dfb57ee45739352e4f6a1020e
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2023-06-02 20:29:29 +0200

    Add mypy to the project

--- PinBoard/settings.py
+++ PinBoard/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import (
     Path,
 )
+from typing import (
+    List,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +30,7 @@ SECRET_KEY = "django-insecure-lpk$it+!m-!d-p@92=u$a187l7%0sr-d*2d3jp)+q+kfhz3l7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
