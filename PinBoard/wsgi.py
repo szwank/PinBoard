bd5257ef9afa5a6d807d68510be89e0b319cf83a
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2023-06-02 20:19:13 +0200

    Add isort to the project

--- PinBoard/wsgi.py
+++ PinBoard/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import (
+    get_wsgi_application,
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ToDoList.settings")
 
