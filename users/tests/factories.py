ccce20f455212da18741ddbee5e257607a085b03
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2023-06-08 12:47:53 +0200

    Fix UserFactory.create_user function
    Force to use active arg instead is_active kwarg.

--- users/tests/factories.py
+++ users/tests/factories.py
@@ -10,8 +10,20 @@ User = get_user_model()
 
 class UserFactory:
     @staticmethod
-    def create_user(**kwargs) -> User:
-        kwargs.setdefault("is_active", True)
+    def create_user(active=True, **kwargs) -> User:
+        """
+        Create user
+
+        :param active: determine if created user will be active.
+            Don't set is_active in kwargs as this will throw an error.
+        :param kwargs: other user values
+        """
+        if kwargs.get("is_active"):
+            raise ValueError(
+                "Cannot pass is_active in kwargs. Pass arg active=True instead."
+            )
+        if active:
+            kwargs["is_active"] = True
 
         user = baker.make("users.User", **kwargs)
         return user
