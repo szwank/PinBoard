b959e3eda5b475f24a8fe322420c98abef5b13ab
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2023-06-08 19:31:04 +0200

    Configure static files for project
    Collect static files in docker compose
    before running app

--- PinBoard/urls.py
+++ PinBoard/urls.py
@@ -14,6 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import (
+    settings,
+)
+from django.conf.urls.static import (
+    static,
+)
 from django.contrib import (
     admin,
 )
@@ -67,4 +73,4 @@ urlpatterns = [
             ]
         ),
     ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
