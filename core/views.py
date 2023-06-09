13b7676d9d501dd8fcaabcfe5acf31a27544ed21
lpankiewicz <lukasz.pankiewicz@profil-software.com>
2023-06-09 12:16:37 +0200

    Replace strings to variables in IndexView.get_context_data

--- core/views.py
+++ core/views.py
@@ -13,18 +13,21 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if not self.request.user.is_anonymous:
-            context["user_tasks"] = self.request.user.tasks()
+            user_tasks = "user_tasks"
+            user_epics = "user_epics"
+
+            context[user_tasks] = self.request.user.tasks()
             task_search = self.request.GET.get("task_search", False)
             if task_search:
-                context["user_tasks"] = (
-                    context["user_tasks"].filter(title__contains=task_search).all()
+                context[user_tasks] = (
+                    context[user_tasks].filter(title__contains=task_search).all()
                 )
 
-            context["user_epics"] = self.request.user.epics()
+            context[user_epics] = self.request.user.epics()
             epic_search = self.request.GET.get("epic_search", False)
             if epic_search:
-                context["user_epics"] = (
-                    context["user_epics"].filter(title__contains=epic_search).all()
+                context[user_epics] = (
+                    context[user_epics].filter(title__contains=epic_search).all()
                 )
         return context
 
